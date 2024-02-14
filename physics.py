70cc34c3d1d5832d41dd7d6f15f822e1599ddf6d
jondeal <87876334+jondeal@users.noreply.github.com>
2024-02-14 14:17:25 -0500

    refactor: module updated to reflect .position attributes of Actor and Tile classes

--- physics.py
+++ physics.py
@@ -6,14 +6,14 @@ def resolve_physics(level):
     while to_resolve:
         for actor in to_resolve:
             while actor.impulse > 0:
-                test_address = (actor.x + actor.direction[0], actor.y + actor.direction[1])
+                test_address = (actor.position[0] + actor.direction[0], actor.position[1] + actor.direction[1])
                 for tile in level.tiles:
-                    if (tile.x, tile.y) == test_address:
+                    if tile.position == test_address:
                         if tile.is_blocked:
                             actor.impulse = 0
                             actor.direction = (0, 0)
                         else:
-                            actor.x, actor.y = tile.x, tile.y
+                            actor.position = tile.position
                             actor.rect = tile.rect
                             actor.impulse -= 1
                             actor.direction = (0, 0)
