71e6d53311071fc10f8c151ef758d881a80d1abd
jondeal <87876334+jondeal@users.noreply.github.com>
2024-02-16 20:10:33 -0500

    refactor: first attempt at recursive collision model

--- physics.py
+++ physics.py
@@ -1,21 +1,28 @@
 def resolve_physics(level):
-    to_resolve = []
+
+    def move_check(entity):
+        position_to_check = (entity.position[0] + entity.direction[0], entity.position[1] + entity.direction[1])
+        for other_actor in level.actors:
+            if other_actor.position == position_to_check:
+                handle_collision(other_actor)
+        for tile in level.tiles:
+            if tile.position == position_to_check:
+                if tile.is_blocked:
+                    handle_collision(tile)
+                else:
+                    entity.position = tile.position
+                    entity.rect = tile.rect
+                    entity.impulse -= 1
+                    entity.direction = (0, 0)
+
+    def handle_collision(other_thing):
+        if other_thing.name == 'wall':
+            actor.direction = (-actor.direction[0], -actor.direction[1])
+        else:
+            other_thing.impulse = actor.impulse
+            other_thing.direction = actor.direction
+            move_check(other_thing)
+
     for actor in level.actors:
         if actor.impulse > 0:
-            to_resolve.append(actor)
-    while to_resolve:
-        for actor in to_resolve:
-            while actor.impulse > 0:
-                test_address = (actor.position[0] + actor.direction[0], actor.position[1] + actor.direction[1])
-                for tile in level.tiles:
-                    if tile.position == test_address:
-                        if tile.is_blocked:
-                            actor.impulse = 0
-                            actor.direction = (0, 0)
-                        else:
-                            actor.position = tile.position
-                            actor.rect = tile.rect
-                            actor.impulse -= 1
-                            actor.direction = (0, 0)
-            else:
-                to_resolve.remove(actor)
+            move_check(actor)
