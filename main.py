9e110efcfa5278817e2852e78c296eda1bfd6ec5
jondeal <87876334+jondeal@users.noreply.github.com>
2024-02-20 00:14:33 -0500

    feature: player can now do (lshift + direction) to dash/shove (increased impulse), while (direction) alone will move/push (impulse = 1)

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ game = Game.Game([], None)
 game.generate_new_level()
 game.current_level = game.levels[0]
 game.current_level.spawn_actor('player', game.current_level.get_random_open_tile())
-game.current_level.spawn_actor('small slime', game.current_level.get_random_open_tile())
+game.current_level.spawn_actor('green slime', game.current_level.get_random_open_tile())
+game.current_level.spawn_actor('red slime', game.current_level.get_random_open_tile())
+game.current_level.spawn_actor('blue slime', game.current_level.get_random_open_tile())
+
 
 player = game.current_level.actors[0]
 
@@ -27,7 +30,10 @@ while running:
                 running = False
             if event.key in constants.move_keys:
                 player.direction = constants.move_keys[event.key]
-                player.impulse += 1
+                if event.mod == pygame.KMOD_LSHIFT:
+                    player.impulse = 3
+                else:
+                    player.impulse = 1
 
     physics.resolve_physics(game.current_level)
     render.render(game.current_level)
