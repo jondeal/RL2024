353943f1f09a3e063ff22da301acbf780f7d443e
jondeal <87876334+jondeal@users.noreply.github.com>
2024-02-20 23:46:32 -0500

    refactor: actor spawn commands  altered to reflect changed slime/mœbus name change

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ game = Game.Game([], None)
 game.generate_new_level()
 game.current_level = game.levels[0]
 game.current_level.spawn_actor('player', game.current_level.get_random_open_tile())
-game.current_level.spawn_actor('green slime', game.current_level.get_random_open_tile())
-game.current_level.spawn_actor('red slime', game.current_level.get_random_open_tile())
-game.current_level.spawn_actor('blue slime', game.current_level.get_random_open_tile())
+game.current_level.spawn_actor('green mœbus', game.current_level.get_random_open_tile())
+game.current_level.spawn_actor('red mœbus', game.current_level.get_random_open_tile())
+game.current_level.spawn_actor('blue mœbus', game.current_level.get_random_open_tile())
 
 
 player = game.current_level.actors[0]
