77bfd0885009a51aaeac9813d6d4f511afa8d013
jondeal <87876334+jondeal@users.noreply.github.com>
2024-02-21 23:09:35 -0500

    feature: small mœbuses now have Mass of 1, player now has Mass 2; added large mœbus with Mass 2

--- actor_templates.py
+++ actor_templates.py
@@ -1,18 +1,27 @@
 actor_templates = [
     {'name': 'player',
      'glyph': '@',
-     'glyph_color': [255, 255, 255, 255]
+     'glyph_color': [255, 255, 255, 255],
+     'mass': 2
      },
     {'name': 'green mœbus',
      'glyph': 'o',
-     'glyph_color': [0, 255, 0, 255]
+     'glyph_color': [0, 255, 0, 255],
+     'mass': 1
      },
     {'name': 'red mœbus',
      'glyph': 'o',
-     'glyph_color': [255, 0, 0, 255]
+     'glyph_color': [255, 0, 0, 255],
+     'mass': 1
      },
     {'name': 'blue mœbus',
      'glyph': 'o',
-     'glyph_color': [0, 0, 255, 255]
+     'glyph_color': [0, 0, 255, 255],
+     'mass': 1
+     },
+    {'name': 'large green mœbus',
+     'glyph': 'O',
+     'glyph_color': [0, 255, 0, 255],
+     'mass': 2
      }
 ]
