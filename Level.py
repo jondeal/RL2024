6bda6598799ab2713684ba19ff44ecc936c36e97
jondeal <87876334+jondeal@users.noreply.github.com>
2024-02-21 23:10:52 -0500

    refactor: spawn_actor function reflects Actor entities having Mass

--- Level.py
+++ Level.py
@@ -64,7 +64,7 @@ class Level:
             if template['name'] == actor_type:
                 new_actor = Actor.Actor(template['name'], destination.position, destination.rect,
                                         template['glyph'], 0, constants.FONT_SIZE, template['glyph_color'],
-                                        0, 0)
+                                        0, 0, template['mass'])
                 self.actors.append(new_actor)
 
     def spawn_terrain(self, terrain_name, limit):
