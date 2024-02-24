f6d0bc3b71aad45f4689947604578545559ac1e4
jondeal <87876334+jondeal@users.noreply.github.com>
2024-02-23 22:13:58 -0500

    refactor: now only spawns a small and a large mœbus; passes player variable to render_ui for rendering player glo_count

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ import physics
 pygame.init()
 
 game = Game.Game([], None)
-game.generate_new_level()
-game.current_level = game.levels[0]
-game.current_level.spawn_actor('player', game.current_level.get_random_open_tile())
-game.current_level.spawn_actor('green mœbus', game.current_level.get_random_open_tile())
-game.current_level.spawn_actor('red mœbus', game.current_level.get_random_open_tile())
-game.current_level.spawn_actor('blue mœbus', game.current_level.get_random_open_tile())
-game.current_level.spawn_actor('large green mœbus', game.current_level.get_random_open_tile())
 
+game.generate_new_level()
+
+game.current_level = game.levels[0]
+
+game.current_level.spawn_actor('player', game.current_level.get_random_open_tile())
+game.current_level.spawn_actor('small mœbus', game.current_level.get_random_open_tile())
+game.current_level.spawn_actor('large mœbus', game.current_level.get_random_open_tile())
 
 player = game.current_level.actors[0]
 
@@ -39,5 +39,5 @@ while running:
 
     physics.resolve_physics(game.current_level)
     render_level.render_level(game.current_level)
-    render_ui.render_ui()
+    render_ui.render_ui(player)
     pygame.display.flip()
