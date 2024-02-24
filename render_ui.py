da950af017f5c6d47f030183611d52fc97575814
jondeal <87876334+jondeal@users.noreply.github.com>
2024-02-23 22:18:08 -0500

    refactor: ui displays player GLO based on the player actor's glo_count instead of a placeholder; some alteration to ui element spacing

--- render_ui.py
+++ render_ui.py
@@ -6,19 +6,14 @@ ui_rect = pygame.Rect(
     (constants.screen_info.current_w - constants.screen_info.current_h, constants.screen_info.current_h)
 )
 
-player_GLO = 10
 
-
-def render_ui():
+def render_ui(player):
     constants.screen.fill(constants.SCREEN_COLOR, ui_rect)
 
-    glo_rect = constants.FONT.render_to(constants.screen, (ui_rect.left + constants.TILE_WIDTH, ui_rect.top),
+    glo_rect = constants.FONT.render_to(constants.screen, (ui_rect.left + constants.TILE_WIDTH,
+                                                           ui_rect.top + constants.TILE_HEIGHT),
                                         'GLO: ', [255, 255, 255, 255], None, size=constants.FONT_SIZE, rotation=0)
 
-    for i in range(player_GLO):
+    for i in range(player.glo_count):
         constants.FONT.render_to(constants.screen, (glo_rect.right + (constants.TILE_WIDTH * i), glo_rect.top),
                                  '*', 'cyan', None, size=constants.FONT_SIZE * 2, rotation=45)
-
-# §  gene
-
-
