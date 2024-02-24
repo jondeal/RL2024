bea954c486488283217d9eeb335dde9ca14a8002
jondeal <87876334+jondeal@users.noreply.github.com>
2024-02-23 23:09:50 -0500

    initial: Item class

---
+++ Item.py
@@ -0,0 +1,14 @@
+class Item:
+    def __init__(self, name, position, rect,
+                 glyph, glyph_rotation, glyph_size, glyph_color,
+                 direction, speed, mass):
+        self.name = name
+        self.position = position
+        self.rect = rect
+        self.glyph = glyph
+        self.glyph_rotation = glyph_rotation
+        self.glyph_size = glyph_size
+        self.glyph_color = glyph_color
+        self.direction = direction
+        self.speed = speed
+        self.mass = mass
