6630b0b971e46b3facb586c2e1ec5031d85a4486
jondeal <87876334+jondeal@users.noreply.github.com>
2024-02-23 22:15:34 -0500

    feature: large and small mœbuses instead of the various colored ones; all templates now have a glo_count for default GLO values on spawn

--- actor_templates.py
+++ actor_templates.py
@@ -2,26 +2,19 @@ actor_templates = [
     {'name': 'player',
      'glyph': '@',
      'glyph_color': [255, 255, 255, 255],
-     'mass': 1
+     'mass': 1,
+     'glo_count': 10
      },
-    {'name': 'green mœbus',
+    {'name': 'small mœbus',
      'glyph': 'o',
      'glyph_color': [0, 255, 0, 255],
-     'mass': 1
+     'mass': 1,
+     'glo_count': 3
      },
-    {'name': 'red mœbus',
-     'glyph': 'o',
-     'glyph_color': [255, 0, 0, 255],
-     'mass': 1
-     },
-    {'name': 'blue mœbus',
-     'glyph': 'o',
-     'glyph_color': [0, 0, 255, 255],
-     'mass': 1
-     },
-    {'name': 'large green mœbus',
+    {'name': 'large mœbus',
      'glyph': 'O',
      'glyph_color': [0, 255, 0, 255],
-     'mass': 2
+     'mass': 2,
+     'glo_count': 6
      }
 ]
