a630a70c7c89faf75e1afe855b18081791984530
jondeal <87876334+jondeal@users.noreply.github.com>
2024-02-26 22:05:35 -0500

    feature: YeetStick

--- item_templates.py
+++ item_templates.py
@@ -3,5 +3,10 @@ item_templates = [
      'glyph': '⚨',
      'glyph_color': [255, 255, 0, 255],
      'mass': 1
+     },
+    {'name': 'YeetStick',
+     'glyph': 'Î',
+     'glyph_color': [255, 255, 0, 255],
+     'mass': 1
      }
 ]
