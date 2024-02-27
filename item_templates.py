c84d6355bb1531a06d183f975678ba591c2c3924
jondeal <87876334+jondeal@users.noreply.github.com>
2024-02-26 23:36:17 -0500

    feature: GenoQuery kiosk; glyph_rotation now loaded from template on item spawn

--- item_templates.py
+++ item_templates.py
@@ -1,12 +1,22 @@
+import random
+
 item_templates = [
     {'name': 'GenoScribe',
      'glyph': '⚨',
      'glyph_color': [255, 255, 0, 255],
+     'glyph_rotation': random.randrange(8) * 45,
      'mass': 1
      },
     {'name': 'YeetStick',
      'glyph': 'Î',
      'glyph_color': [255, 255, 0, 255],
+     'glyph_rotation': random.randrange(8) * 45,
      'mass': 1
+     },
+    {'name': 'GenoQuery',
+     'glyph': '⌂',
+     'glyph_color': [255, 255, 0, 255],
+     'glyph_rotation': 0,
+     'mass': 3
      }
 ]
