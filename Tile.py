5e7fd3ee384593f575b6a79761a81d6f3df3ac13
jondeal <87876334+jondeal@users.noreply.github.com>
2024-02-28 16:35:44 -0500

    refactor: Rearranged attribute order; feature: Tiles now have glyph_size_modifier attribute

--- Tile.py
+++ Tile.py
@@ -1,11 +1,14 @@
 class Tile:
-    def __init__(self, name, position, rect, glyph, glyph_rotation, glyph_size, glyph_color, bg_color, is_blocked):
+    def __init__(self, name, position, rect,
+                 glyph, glyph_color, glyph_size, glyph_size_modifier, glyph_rotation, bg_color,
+                 is_blocked):
         self.name = name
         self.position = position
         self.rect = rect
         self.glyph = glyph
-        self.glyph_rotation = glyph_rotation
-        self.glyph_size = glyph_size
         self.glyph_color = glyph_color
+        self.glyph_size = glyph_size
+        self.glyph_size_modifier = glyph_size_modifier
+        self.glyph_rotation = glyph_rotation
         self.bg_color = bg_color
         self.is_blocked = is_blocked
