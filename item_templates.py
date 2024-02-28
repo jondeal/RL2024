2056875e44aed43f8feb5f72476816c71b1f7164
jondeal <87876334+jondeal@users.noreply.github.com>
2024-02-28 16:37:59 -0500

    feature: glyph_size_modifiers added to each template

--- item_templates.py
+++ item_templates.py
@@ -4,18 +4,21 @@ item_templates = [
     {'name': 'GenoScribe',
      'glyph': '⚨',
      'glyph_color': [255, 255, 0, 255],
+     'glyph_size_modifier': 1,
      'glyph_rotation': random.randrange(8) * 45,
      'mass': 1
      },
     {'name': 'YeetStick',
      'glyph': 'Î',
      'glyph_color': [255, 255, 0, 255],
+     'glyph_size_modifier': 1,
      'glyph_rotation': random.randrange(8) * 45,
      'mass': 1
      },
     {'name': 'GenoQuery',
      'glyph': '⌂',
      'glyph_color': [255, 255, 0, 255],
+     'glyph_size_modifier': 1.75,
      'glyph_rotation': 0,
      'mass': 3
      }
