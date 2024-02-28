e93fd1d9bc8cf91f022633293369126c38737b68
jondeal <87876334+jondeal@users.noreply.github.com>
2024-02-28 10:55:50 -0500

    feature: Rudimentary player inventory display

--- render_ui.py
+++ render_ui.py
@@ -17,3 +17,17 @@ def render_ui(player):
     for i in range(player.glo_count):
         constants.FONT.render_to(constants.screen, (glo_rect.right + (constants.TILE_WIDTH * i), glo_rect.top),
                                  '*', 'cyan', None, size=constants.FONT_SIZE * 2, rotation=45)
+
+    inventory, inventory_rect = constants.FONT.render('INVENTORY', [255, 255, 255, 255], None, size=constants.FONT_SIZE, rotation=0)
+    inventory_rect.centerx = ui_rect.centerx
+    inventory_rect.top = ui_rect.top + constants.TILE_HEIGHT * 3
+    inventory_rect.bottom = ui_rect.top + constants.TILE_HEIGHT * 4
+    constants.screen.blit(inventory, inventory_rect)
+
+    if player.inventory:
+        for item in player.inventory:
+            inventory_item, inventory_item_rect = constants.FONT.render(item.glyph + ' : ' + item.name,
+                                                                        [255, 255, 0, 255], None,
+                                                                        size=constants.FONT_SIZE, rotation=0)
+            constants.screen.blit(inventory_item, (ui_rect.left + constants.TILE_WIDTH, inventory_rect.bottom + constants.TILE_HEIGHT +
+                                                   constants.TILE_HEIGHT * player.inventory.index(item)))
