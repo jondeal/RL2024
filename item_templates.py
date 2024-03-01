a2c400d9f6ab3617a923756695892cff62e2a035
jondeal <87876334+jondeal@users.noreply.github.com>
2024-02-29 23:20:10 -0500

    config: Changed YeetStick glyph to lowercase

--- item_templates.py
+++ item_templates.py
@@ -9,7 +9,7 @@ item_templates = [
      'mass': 1
      },
     {'name': 'YeetStick',
-     'glyph': 'Î',
+     'glyph': 'î',
      'glyph_color': [255, 255, 0, 255],
      'glyph_size_modifier': 1,
      'glyph_rotation': random.randrange(8) * 45,
