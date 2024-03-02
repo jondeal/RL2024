e9c5297784184edae0f902e474541f35d202f98c
jondeal <87876334+jondeal@users.noreply.github.com>
2024-03-02 00:16:17 -0500

    refactor: Grouped and re-arranged some variables and statements in a more logical way

--- constants.py
+++ constants.py
@@ -2,6 +2,8 @@ import bools
 import pygame.freetype
 import os
 
+import constants
+
 pygame.freetype.init()
 pygame.display.init()
 pygame.display.set_caption('RL2024')
@@ -12,6 +14,11 @@ screen_info = pygame.display.Info()
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d, %d" % (screen_info.current_w - screen_info.current_h, 0)
 # os.environ['SDL_VIDEO_WINDOW_POS'] = "%d, %d" % (screen_info.current_w / 4, 0)  # for being able to see the console; disable render_ui
 
+if bools.is_fullscreen:
+    screen = pygame.display.set_mode((screen_info.current_w, screen_info.current_h), pygame.FULLSCREEN)
+else:
+    screen = pygame.display.set_mode((screen_info.current_h, screen_info.current_h), pygame.NOFRAME)
+
 X_RANGE = 20
 Y_RANGE = 20
 
@@ -20,14 +27,10 @@ TILE_WIDTH = TILE_HEIGHT
 
 LEVEL_RECT = pygame.Rect(0, 0, X_RANGE * TILE_WIDTH, Y_RANGE * TILE_HEIGHT)
 UI_RECT = pygame.Rect(screen_info.current_h, 0, screen_info.current_w - screen_info.current_h, screen_info.current_h)
+PROMPT_RECT = pygame.Rect(0, screen_info.current_h - constants.TILE_WIDTH, LEVEL_RECT.width, constants.TILE_HEIGHT)
 
 FONT_SIZE = TILE_WIDTH
 # FONT = pygame.freetype.SysFont('juliamono', FONT_SIZE)
 FONT = pygame.freetype.Font('/usr/share/fonts/truetype/julia/JuliaMono-Light.ttf', FONT_SIZE)
 
 SCREEN_COLOR = (0, 0, 35)
-
-if bools.is_fullscreen:
-    screen = pygame.display.set_mode((screen_info.current_w, screen_info.current_h), pygame.FULLSCREEN)
-else:
-    screen = pygame.display.set_mode((screen_info.current_h, screen_info.current_h), pygame.NOFRAME)
