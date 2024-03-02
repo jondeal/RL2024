30281f3562bb0aab032f003c0a31f16fdd91ff94
jondeal <87876334+jondeal@users.noreply.github.com>
2024-03-01 22:08:28 -0500

    feature: rects for level and ui now defined within constants.py

--- constants.py
+++ constants.py
@@ -12,12 +12,14 @@ screen_info = pygame.display.Info()
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d, %d" % (screen_info.current_w - screen_info.current_h, 0)
 # os.environ['SDL_VIDEO_WINDOW_POS'] = "%d, %d" % (screen_info.current_w / 4, 0)  # for being able to see the console; disable render_ui
 
-
 X_RANGE = 20
 Y_RANGE = 20
 
-TILE_HEIGHT = screen_info.current_h // Y_RANGE
+TILE_HEIGHT = screen_info.current_h // (Y_RANGE + 1)  # int following Y_RANGE is num lines to make the message bar below the level display
 TILE_WIDTH = TILE_HEIGHT
+
+LEVEL_RECT = pygame.Rect(0, 0, X_RANGE * TILE_WIDTH, Y_RANGE * TILE_HEIGHT)
+UI_RECT = pygame.Rect(screen_info.current_h, 0, screen_info.current_w - screen_info.current_h, screen_info.current_h)
 
 FONT_SIZE = TILE_WIDTH
 # FONT = pygame.freetype.SysFont('juliamono', FONT_SIZE)
@@ -29,6 +31,3 @@ if bools.is_fullscreen:
     screen = pygame.display.set_mode((screen_info.current_w, screen_info.current_h), pygame.FULLSCREEN)
 else:
     screen = pygame.display.set_mode((screen_info.current_h, screen_info.current_h), pygame.NOFRAME)
-
-# directions order: upper left, left, lower left, up, down, upper right, right, lower right
-# directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
