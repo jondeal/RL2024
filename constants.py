d1edca1fce68f189bc9276c4b6bbdb7b48b65c5c
jondeal <87876334+jondeal@users.noreply.github.com>
2024-03-03 01:58:23 -0500

    chore: Whitespace and comment changes

--- constants.py
+++ constants.py
@@ -11,6 +11,7 @@ pygame.mouse.set_visible(False)
 
 
 screen_info = pygame.display.Info()
+
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d, %d" % (screen_info.current_w - screen_info.current_h, 0)
 # os.environ['SDL_VIDEO_WINDOW_POS'] = "%d, %d" % (screen_info.current_w / 4, 0)  # for being able to see the console; disable render_ui
 
@@ -22,7 +23,8 @@ else:
 X_RANGE = 20
 Y_RANGE = 20
 
-TILE_HEIGHT = screen_info.current_h // (Y_RANGE + 1)  # int following Y_RANGE is num lines to make the message bar below the level display
+# int following Y_RANGE is num lines to make room for the message bar below the level display
+TILE_HEIGHT = screen_info.current_h // (Y_RANGE + 1)
 TILE_WIDTH = TILE_HEIGHT
 
 LEVEL_RECT = pygame.Rect(0, 0, X_RANGE * TILE_WIDTH, Y_RANGE * TILE_HEIGHT)
