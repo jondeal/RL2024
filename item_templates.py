046dd4670433f7b04001986eed045863d886dc89
jondeal <87876334+jondeal@users.noreply.github.com>
2024-03-05 20:56:33 -0500

    feat: Templates account for item.inventory attribute

--- item_templates.py
+++ item_templates.py
@@ -6,20 +6,23 @@ item_templates = [
      'glyph_color': [255, 255, 0, 255],
      'glyph_size_modifier': 1,
      'glyph_rotation': random.randrange(8) * 45,
-     'mass': 1
+     'mass': 1,
+     'inventory': []
      },
     {'name': 'YeetStick',
      'glyph': 'î',
      'glyph_color': [255, 255, 0, 255],
      'glyph_size_modifier': 1,
      'glyph_rotation': random.randrange(8) * 45,
-     'mass': 1
+     'mass': 1,
+     'inventory': None
      },
     {'name': 'GenoQuery',
      'glyph': '⌂',
      'glyph_color': [255, 255, 0, 255],
      'glyph_size_modifier': 1.75,
      'glyph_rotation': 0,
-     'mass': 3
+     'mass': 3,
+     'inventory': None
      }
 ]
