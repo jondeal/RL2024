95490608de5821a808d4a426166ff6a608a40100
jondeal <87876334+jondeal@users.noreply.github.com>
2024-03-07 00:43:14 -0500

    feat: Gene class has is_selected attribute

--- Gene.py
+++ Gene.py
@@ -1,4 +1,5 @@
 class Gene:
-    def __init__(self, name, ability):
+    def __init__(self, name, ability, is_selected):
         self.name = name
         self.ability = ability
+        self.is_selected = is_selected
