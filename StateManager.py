accedfabcef10cf1144f0cea016cfe84d7868972
jondeal <87876334+jondeal@users.noreply.github.com>
2024-03-08 00:01:44 -0500

    Initial commit

---
+++ StateManager.py
@@ -0,0 +1,18 @@
+from ChoosingActionState import ChoosingActionState
+
+
+class StateManager:
+    def __init__(self, game, player):
+        self.game = game
+        self.player = player
+        self.previous_state = None
+        self.current_state = ChoosingActionState(game, player)
+
+    def change_state(self, new_state):
+        self.current_state.exit()
+        self.previous_state = self.current_state
+        self.current_state = new_state
+        self.current_state.enter()
+
+    def update(self, events):
+        self.current_state.update(events)
