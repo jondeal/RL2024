acc416821f8d3b053e403994bad01acc5681cfd4
jondeal <87876334+jondeal@users.noreply.github.com>
2024-03-12 09:59:59 -0400

    chore: removed deprecated is_blocked attribute

--- tile_templates.py
+++ tile_templates.py
@@ -4,15 +4,13 @@ tile_templates = [
      'glyph_color': [150, 150, 150, 255],
      'glyph_size_modifier': 1,
      'bg_base_rgba': [80, 50, 20, 255],
-     'bg_rgb_range': [(-10, 10), (-10, 10), (-10, 10)],
-     'is_blocked': False
+     'bg_rgb_range': [(-10, 10), (-10, 10), (-10, 10)]
      },
     {'name': 'wall',
      'glyph': '#',
      'glyph_color': [100, 100, 100, 255],
      'glyph_size_modifier': 1,
      'bg_base_rgba': [0, 0, 0, 255],
-     'bg_rgb_range': [(230, 255), (155, 200), (90, 165)],
-     'is_blocked': True
+     'bg_rgb_range': [(230, 255), (155, 200), (90, 165)]
      }
 ]
