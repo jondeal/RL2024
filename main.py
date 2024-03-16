a430b0af2ef11efaf5cf77346696d2b7a2089710
jondeal <87876334+jondeal@users.noreply.github.com>
2024-03-15 22:46:55 -0400

    refactor: Simplified turn passing between player and AI actors, removing extraneous conditionals

--- main.py
+++ main.py
@@ -50,18 +50,15 @@ while running:
             if event.key == controls.keybinds['quit'] and event.mod == controls.keybinds['mod key']:
                 running = False
     if player.turn_complete is False:
-        for actor in game.current_level.actors[1:]:
-            actor.turn_complete = False
         state_manager.update(events)
     else:
         for actor in game.current_level.actors[1:]:
-            if actor.turn_complete is False:
-                if 'can_move' in actor.abilities:
-                    direction_list = [direction[1] for direction in controls.direction_keys.values()]
-                    actor.direction = random.choice(direction_list)
-                    actor.move()
-                else:
-                    pass
+            if 'can_move' in actor.abilities:
+                direction_list = [direction[1] for direction in controls.direction_keys.values()]
+                actor.direction = random.choice(direction_list)
+                actor.move()
+            else:
+                pass
         player.turn_complete = False
     physics.resolve_physics(game.current_level)
     animation.update()
