766f8fa477ac28aab8724af7f989cd4e83c99151
jondeal <87876334+jondeal@users.noreply.github.com>
2024-03-19 19:20:21 -0400

    feat: Added update function for animating entities with each pass through the gameloop rather than only when in the animation state

--- animation.py
+++ animation.py
@@ -1,4 +1,14 @@
-def update():
+import random
+
+
+# def update(level):  # example of how to do animation updates for color-shifting glyphs, etc.
+#     for tile in level.tiles:
+#         if tile.position == (0, 0):
+#             randos = ['a', 'b', 'c']
+#             tile.glyph = random.choice(randos)
+
+
+def animate():
     if entities_to_animate:
         entity = entities_to_animate[0][0]
         rects_traversed = entities_to_animate[0][1]
