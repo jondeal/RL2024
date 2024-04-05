69b5c747ba41f22badefe1083aff6522637b3750
jondeal <87876334+jondeal@users.noreply.github.com>
2024-04-04 23:47:14 -0400

    Duplicate commit for file changes not picked up by previous push

--- ChoosingActionState.py
+++ ChoosingActionState.py
@@ -19,18 +19,20 @@ class ChoosingActionState(State):
     def update(self, events):
         for event in events:
             if event.type == pygame.KEYUP:
-                if event.key == pygame.K_LSHIFT:
+                if event.key == controls.keybinds['mod key']:
                     pass
                 else:
                     render_ui.prompt_to_render = None
-                if event.key in controls.direction_keys:
-                    self.player.direction = controls.direction_keys[event.key][1]
-                    self.player.move(self.game.current_level)
-                    if event.mod == controls.keybinds['mod key']:
-                        self.player.shove(self.game.current_level)
-                elif event.key == controls.keybinds['pickup']:
-                    self.player.pickup(self.game.current_level)
-                elif event.key == controls.keybinds['drop']:
-                    self.game.state_manager.change_state(DroppingItemState(self.game, self.player))
-                elif event.key == controls.keybinds['apply']:
-                    self.game.state_manager.change_state(ApplyingItemState(self.game, self.player))
+                    if event.key in controls.direction_keys:
+                        self.player.direction = controls.direction_keys[event.key][1]
+                        keys = pygame.key.get_pressed()
+                        if keys[controls.keybinds['mod key']]:
+                            self.player.shove(self.game.current_level)
+                        else:
+                            self.player.move(self.game.current_level)
+                    elif event.key == controls.keybinds['pickup']:
+                        self.player.pickup(self.game.current_level)
+                    elif event.key == controls.keybinds['drop']:
+                        self.game.state_manager.change_state(DroppingItemState(self.game, self.player))
+                    elif event.key == controls.keybinds['apply']:
+                        self.game.state_manager.change_state(ApplyingItemState(self.game, self.player))
