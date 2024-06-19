c87b40db9fc312937765e27c886617714401a998
jondeal <87876334+jondeal@users.noreply.github.com>
2024-06-18 21:45:36 -0400

    Initial commit

--- animation.py
+++ animation.py
@@ -1,30 +1,9 @@
-import random
-
-
-# def update(level):  # example of how to do animation updates for color-shifting glyphs, etc.
-#     for tile in level.tiles:
-#         if tile.position == (0, 0):
-#             randos = ['a', 'b', 'c']
-#             tile.glyph = random.choice(randos)
+animation_events = []
 
 
 def animate(level):
-    if entities_to_animate:
-        entity = entities_to_animate[0][0]
-        rects_traversed = entities_to_animate[0][1]
-        if rects_traversed:
-            entity.rect = rects_traversed[0][0]
-            entity.glyph_rotation = rects_traversed[0][1]
-            rects_traversed.remove(rects_traversed[0])
+    for animation_event in animation_events:
+        if animation_event.is_done is False:
+            animation_event.update()
         else:
-            entities_to_animate.remove(entities_to_animate[0])
-            if entity.name == 'force bolt':
-                if entity in level.items:
-                    level.items.remove(entity)
-                else:
-                    pass
-    else:
-        pass
-
-
-entities_to_animate = []
+            pass
