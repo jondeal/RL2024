77e130353ae8309ae5b31bd71fa0438ebfe3c892
jondeal <87876334+jondeal@users.noreply.github.com>
2024-06-21 02:14:08 -0400

    feature: Game class now has player_state_manager parameter

--- Game.py
+++ Game.py
@@ -3,10 +3,11 @@ import Level
 
 class Game:
 
-    def __init__(self, levels, current_level, state_manager):
+    def __init__(self, levels, current_level, game_state_manager, player_state_manager):
         self.levels = levels
         self.current_level = current_level
-        self.state_manager = state_manager
+        self.game_state_manager = game_state_manager
+        self.player_state_manager = player_state_manager
 
     def generate_new_level(self):
         new_level = Level.Level([], [], [])
