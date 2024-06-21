b82f089a973f49950bf05727eebe78da2b066ef5
jondeal <87876334+jondeal@users.noreply.github.com>
2024-06-21 02:19:35 -0400

    refactor: Added animation event to move_to_position function when entity.speed == 0, which prevents entity from rendering short of its final intended rect

--- physics.py
+++ physics.py
@@ -22,6 +22,9 @@ def resolve_physics(level):
                                             0, 2, "move", False)
         animation.animation_events.append(new_animation)
         if initiating_entity.speed == 0:
+            new_animation = Animation.Animation(initiating_entity, initiating_entity.rect, receiving_entity.rect,
+                                                0, 2, "move", False)
+            animation.animation_events.append(new_animation)
             make_still(initiating_entity)
             check_physics_resolved()
 
