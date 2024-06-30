3bdb03e65532d6c875fc3f5c02c6ad973efbb192
jondeal <87876334+jondeal@users.noreply.github.com>
2024-06-29 23:33:44 -0400

    feat: Animations now have a direction attribute so glyphs can be rotated appropriately during animation

--- Animation.py
+++ Animation.py
@@ -1,11 +1,13 @@
 class Animation:
     def __init__(self, entity_to_animate,
+                 entity_to_animate_direction,
                  start_rect, end_rect,
                  current_frame, total_frames,
                  animation_to_apply,
                  is_done
                  ):
         self.entity_to_animate = entity_to_animate
+        self.entity_to_animate_direction = entity_to_animate_direction
         self.start_rect = start_rect
         self.end_rect = end_rect
         self.current_frame = current_frame
@@ -14,6 +16,11 @@ class Animation:
         self.is_done = is_done
 
     def update(self):
+        if self.entity_to_animate.name == 'force bolt':
+            directions = [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)]
+            for direction in directions:
+                if self.entity_to_animate_direction == direction:
+                    self.entity_to_animate.glyph_rotation = 45 * directions.index(direction)
         if self.animation_to_apply == 'move':
             x_diff = self.end_rect.centerx - self.start_rect.centerx
             y_diff = self.end_rect.centery - self.start_rect.centery
